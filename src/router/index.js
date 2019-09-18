65f4bc28ab207dce594aae360905d22a4b7c5db1
libaidong <1134595193@qq.com>
2019-09-18 22:30:47 +0800

    刚开始初始化,添加了路由

---
+++ src/router/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+// 配置路透表
+const router = new VueRouter({
+  routers: []
+})
+
+// 导出路由实例 挂载到main上
+export default router
